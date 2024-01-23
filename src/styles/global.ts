fbb5e509edf4c24cb6c91b58ec2a3fe8241bf25e
Miguel <miguelarciagaa@gmail.com>
2024-01-23 15:24:15 +0800

    global colors

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,5 @@
 import styled from "styled-components/native";
 
-
 export const StyledHeader1 = styled.Text`
     font-size: 28px
 `
@@ -16,3 +15,12 @@ export const StyledHeader3 = styled.Text`
 export const StyledBodyText = styled.Text`
     font-size: 17px
 `
+
+export const appColors = {
+    green: '#8AC186',
+    yellow: '#FFD47F',
+    violet: '#7768D6',
+    background: '#22232C',
+    darkBlue: '#17181C',
+    white: '#FAFAFA'
+} 
